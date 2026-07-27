3cb4c40ae5bf73a41b3750efeb8dc8a6df315f5b
Vinicius Santos <72794700+vini-dev96@users.noreply.github.com>
2026-07-27 18:58:28 -0300

    adiciona validações de entrada (400)

--- src/routes/cashbackRoutes.js
+++ src/routes/cashbackRoutes.js
@@ -4,6 +4,16 @@ import { calcCashback } from "../services/cashbackService.js";
 
 const router = Router();
 router.post('/cashback', (req, res) => {
+    const { itens } = req.body;
+    if (!itens) {
+        return res.status(400).json({ erro: "Traga um ID de produto válido" })
+    }
+
+    const QuantidadeNegativa = itens.some(i => i.quantidade < 0);
+    if (QuantidadeNegativa) {
+        return res.status(400).json({ erro: "Quantidade menor que zero não é permitido" })
+    }
+
     const resultado = calcCashback(req.body);
     res.json(resultado);
 });
