d834929408dbee0ca4a2458218a223bd7b096cd3
Vinicius Santos <72794700+vini-dev96@users.noreply.github.com>
2026-07-28 00:10:05 -0300

    adicionar ";" por convenção

--- src/repository/produtoRepositoryJson.js
+++ src/repository/produtoRepositoryJson.js
@@ -1,4 +1,4 @@
-import produtos from "../data/base-itens.json" with { type: "json" }
+import produtos from "../data/base-itens.json" with { type: "json" };
 
 async function buscarPorId(id) {
     return produtos.find(p => p.id === id);
